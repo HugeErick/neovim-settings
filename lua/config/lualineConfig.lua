de4b99d28e6f2bff0297f084b5a108c9112d2597
HugeErick <erick.parada101@gmail.com>
2026-04-28 13:37:00 -0600

    showing working path on file edit

--- lua/config/lualineConfig.lua
+++ lua/config/lualineConfig.lua
@@ -1,8 +1,23 @@
 require("lualine").setup({
   options = {
-    theme = "auto", 
+    theme = "auto",
     icons_enabled = true,
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch" },
+    lualine_c = {
+      {
+        "filename",
+        path = 1,
+        shorting_target = 30,
+      },
+    },
+    lualine_d = { "diagnostics" },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
   },
 })
