cd883fc12e97a78e55d535320bafcda191bb707b
HugeErick <erick.parada101@gmail.com>
2026-07-25 18:59:04 -0600

    templates working independetly of the file creation method

--- lua/config/templates.lua
+++ lua/config/templates.lua
@@ -1,62 +1,67 @@
 -- define the templates as tables of strings
 local templates = {
-	cpp = {
-		"#include <iostream>",
-		"#include <string>",
-		"#include <map>",
-		"#include <vector>",
-		"#include <algorithm>",
-		"#include <iterator>",
-		"#include <exception>",
-		"#include <bitset>",
-		"#include <sstream>",
-		"#include <cstdio>",
-		"",
-		"#define ll long long",
-		"",
-		"using namespace std;",
-		"",
-		"int main() {",
-		"  // templ generation",
-		"  return 0;",
-		"}",
-	},
-	c = {
-		"#include <stdio.h>",
-		"",
-		"int main() {",
-		'  printf("Hello world!\\n");',
-		"",
-		"  return 0;",
-		"}",
-	},
+  cpp = {
+    "#include <iostream>",
+    "#include <string>",
+    "#include <map>",
+    "#include <vector>",
+    "#include <algorithm>",
+    "#include <iterator>",
+    "#include <exception>",
+    "#include <bitset>",
+    "#include <sstream>",
+    "#include <cstdio>",
+    "",
+    "#define ll long long",
+    "",
+    "using namespace std;",
+    "",
+    "int main() {",
+    "  // templ generation",
+    "  return 0;",
+    "}",
+  },
+
+  c = {
+    "#include <stdio.h>",
+    "",
+    "int main() {",
+    '  printf("Hello world!\\n");',
+    "",
+    "  return 0;",
+    "}",
+  },
 }
 
 -- create the autocommand
 local template_group = vim.api.nvim_create_augroup("CppTemplates", { clear = true })
 
-vim.api.nvim_create_autocmd("BufNewFile", {
-	group = template_group,
-	pattern = { "*.cpp", "*.c" },
-	callback = function(args)
-		local ext = vim.fn.expand("%:e")
-		local lines = templates[ext]
+vim.api.nvim_create_autocmd({"BufNewFile", "BufReadPost"}, {
+  group = template_group,
+  pattern = { "*.cpp", "*.c" },
+  callback = function(args)
+    local ext = vim.fn.expand("%:e")
+    local lines = templates[ext]
 
-		if lines then
-			-- we use vim.schedule to wait for the buffer to be ready
-			vim.schedule(function()
-				if vim.api.nvim_buf_is_valid(args.buf) then
-					-- force modifiable just in case
-					vim.bo[args.buf].modifiable = true
+    if not lines then
+      return
+    end
 
-					-- insert the lines starting at line 0
-					vim.api.nvim_buf_set_lines(args.buf, 0, -1, false, lines)
+    vim.schedule(function()
+      if not vim.api.nvim_buf_is_valid(args.buf) then
+        return
+      end 
 
-					-- move the cursor to the "code here" line (line 4, column 4)
-					-- note: api uses 0-indexed rows, so 3 is the 4th line.
-					pcall(vim.api.nvim_win_set_cursor, 0, { 4, 4 })
-				end
-			end)
-		end
-	end,
+      local buf_lines = vim.api.nvim_buf_get_lines(args.buf, 0, -1, false)
+      local is_empty = #buf_lines == 0 or (#buf_lines == 1 and buf_lines[1] == "")
+
+      if is_empty then
+        vim.bo[args.buf].modifiable = true
+        vim.api.nvim_buf_set_lines(args.buf, 0, -1, false, lines)
+
+        -- Move cursor if window is valid
+        pcall(vim.api.nvim_win_set_cursor, 0, { 4, 4 })
+      end
+    end)
+  end,
 })
